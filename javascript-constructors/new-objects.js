791ba42e489cd93f0138e8d497b1ff16dbdc5d74
joshuapreciado <preciadojoshua340@yahoo.com>
2022-08-24 00:58:02 +0000

    Completed JS Constructors

---
+++ javascript-constructors/new-objects.js
@@ -0,0 +1,23 @@
+var aObject = new Object();
+console.log('value of an object:', aObject);
+console.log('typeof aobject:', typeof aObject);
+
+var aArray = new Array();
+console.log('value of aArray:', aArray);
+console.log('typeof aArray:', typeof aArray);
+
+var aFunction = new Function();
+console.log('value of aFunction:', aFunction);
+console.log('typeof aFunction:', typeof aFunction);
+
+var aString = new String();
+console.log('value of aString:', aString);
+console.log('typeof aString:', typeof aString);
+
+var aNumber = new Number();
+console.log('value of aNumber:', aNumber);
+console.log('typeof aNumber:', typeof aNumber);
+
+var aBoolean = new Boolean();
+console.log('value of aBoolean:', aBoolean);
+console.log('typeof aBoolean:', typeof aBoolean);
