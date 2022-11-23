d11141a9b3dc8ceb7dfb884e0251120eaf765512
joshuapreciado <preciadojoshua340@yahoo.com>
2022-11-23 06:21:31 +0000

    Completed node-mod systeM

---
+++ node-module-system/a-wrapped-module.js
@@ -0,0 +1,14 @@
+console.log('Typeof Exports:', typeof exports);
+console.log('Value Of Exports:', exports);
+
+console.log('Typeof require:', typeof require);
+console.log('Value Of Require:', require);
+
+console.log('Typeof module:', typeof module);
+console.log('Value of module:', module);
+
+console.log('Typeof __filename:', typeof __filename);
+console.log('Value of __filename:', __filename);
+
+console.log('Typeof __dirname:', typeof __dirname);
+console.log('Value of __dirname:', __dirname);
